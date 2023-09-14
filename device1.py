54e7908cb57d5f32458795d249ec5d961ec30e27
Bruno Cesar de Oliveira Franco <brunocesarfranco@gmail.com>
2023-09-14 20:36:11 -0300

    Create function "mensagem"
    - Criado a função "mensagem" para retornar o payload da mensagem assim que tiver uma assinatura no tópico "aquecedor";

--- device1.py
+++ device1.py
@@ -3,10 +3,19 @@ import time
 from hal import temperatura, umidade, aquecedor
 from definitions import user, password, client_id, server, port
 
+def mensagem(client, user, msg):
+    if msg.topic == 'pucpr/iotmc/brunofranco/aquecedor':
+        aquecedor(msg.payload.decode())
+
 # Conexão Inicial
 client = mqtt.Client(client_id)
 client.username_pw_set(user, password)
 client.connect(server, port)
+
+# Assinatura do tópico
+client.on_message = mensagem
+client.subscribe('pucpr/iotmc/brunofranco/#')
+client.loop_start()
 
 # Comportamento do Sistema
 while True:
